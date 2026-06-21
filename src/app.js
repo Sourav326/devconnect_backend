0c6837442f67b5e8219a5f72bfbda6bb7f3ccf32
Sourav Chauhan <souravchauhan1964@gmail.com>
2026-06-21 21:29:24 +0530

    created single user view and user update API

--- src/app.js
+++ src/app.js
@@ -56,6 +56,62 @@ app.get("/feeds",async(req,res) => {
 //API for handling feeds END ************************
 
 
+//API for handling view single user START ****************
+app.get('/user',async (req,res) => {
+    try{
+        if(req.body){
+            const userId = req.body.id;
+            const user = await User.findById(userId)
+            res.status(200).json({
+                status:true,
+                message:"User fetched succesfully",
+                data: user
+            })
+        } else {
+            res.status(404).json({
+                status:true,
+                message:"No user found",
+                data:[]
+            })
+            
+        }
+    } catch(err){
+        res.status(500).json({
+            status:false,
+            message:"Internal server error"
+        })
+    }
+
+})
+//API for handling view single user END ****************
+
+
+//API for handling update a user START *******************
+app.patch('/user',async(req,res) => {
+    try{
+        if(req.body){
+            const reqData = req.body 
+            const user = await User.findByIdAndUpdate(reqData.id,reqData);
+            res.status(200).json({
+                status:true,
+                message:"User updated succesfully",
+            })
+        } else {
+            res.status(404).json({
+                status:true,
+                message:"No user found"
+            })
+        }
+    }catch(err){
+        res.status(500).json({
+            status:false,
+            message:"Internal server error"
+        })
+    }
+})
+//API for handling update a user END *******************
+
+
 connectDb().then(() => {//first connect to database
     console.log("Database connected successfully");
     app.listen(7080,()=>{//secondly start the server after DB connected sucessfully
